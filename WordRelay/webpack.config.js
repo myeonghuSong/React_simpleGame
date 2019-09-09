907682938bf95256aa4c73426cdecbdb701484d0
myeonghuSong <21300390@handong.edu>
2019-09-09 19:11:22 +0900

    webpack을 활용한 browser 지원 범위 적용

--- WordRelay/webpack.config.js
+++ WordRelay/webpack.config.js
@@ -17,7 +17,14 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [
+                    ['@babel/preset-env',{
+                        targets: {
+                            browsers: ['> 1% in KR'], // browserslist
+                        },
+                        debug: true,
+                    }], 
+                    '@babel/preset-react'],
                 plugins: ['@babel/plugin-proposal-class-properties']
             },
         }],
