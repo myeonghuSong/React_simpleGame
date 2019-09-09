57090c94c596b2a88e6306c87576f0a00c12be9d
myeonghuSong <21300390@handong.edu>
2019-09-09 17:41:32 +0900

    구구단 webpack 적용

--- GuGuDan/client.jsx
+++ GuGuDan/client.jsx
@@ -1,2 +1,6 @@
 const React = require('react');
-const ReactDom = require('react-dom');
+const ReactDOM = require('react-dom');
+
+const GuGuDan = require('./GuGuDan');
+
+ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
