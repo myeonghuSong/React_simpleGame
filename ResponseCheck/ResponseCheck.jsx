978a6d495eddb3afc5e6b568f57c3a031f11027e
myeonghuSong <21300390@handong.edu>
2019-09-10 16:46:49 +0900

    반응속도 체크 게임 로직

---
+++ ResponseCheck/ResponseCheck.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+class ResponseCheck extends Component {
+    state = {
+        state: 'waiting',
+        message: '클릭해서 시작하세요',
+        result: [],
+    };
+    timeout;
+    startTime;
+    endTime;
+
+    onClickScreen = () => {
+        const { state, message } = this.state;
+        if(state === 'waiting'){
+            this.setState({
+                state: 'ready',
+                message: '초록색이되면 클릭하세요',
+            });
+            this.timeout = setTimeout( () => {
+                this.setState({
+                    state: 'now',
+                    message: '지금 클릭',
+                });
+                this.startTime = new Date();
+            }, Math.floor(Math.random() * 1000 ) + 2000); //2초 ~ 3초
+        } else if( state === 'ready' ){ //성급하게 클릭
+            clearTimeout(this.timeout);
+            this.setState({
+                state: 'waiting',
+                message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+            });
+        } else if( state === now ){
+            this.endTime = new Date();
+            this.setState((prevState)=>{
+                return{
+                    state: 'waiting',
+                    message: '클릭해서 시작하세요',
+                    result: [...prevState.result, this.endTime - this.startTime],
+                }
+            });
+        }
+    };
+
+    renderAverage = () => {
+        return this.state.result.length === 0
+        ? null
+        : <div>평균시간 : {this.state.result.reduce((a, c) => a + c ) / this.state.result.length}ms</div>
+    }
+
+    render() {
+        const { state, message } = this.state;
+        return (
+            <>
+                <div
+                    id="screen"
+                    className={state}
+                    onClick={onClickScreen}
+                >
+                    {message}
+                </div>
+                {this.renderAverage()}
+            </>
+        );
+    }
+}
+
+export default ResponseCheck;
