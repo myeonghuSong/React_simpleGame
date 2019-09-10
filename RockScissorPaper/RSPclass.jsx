af18473b36363057743ea494d1b4b7c32972455a
myeonghuSong <21300390@handong.edu>
2019-09-10 21:22:56 +0900

    RSP class에서 hooks로 전환

--- RockScissorPaper/RSPclass.jsx
+++ RockScissorPaper/RSPclass.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { Component } from 'react';
 
 //클래스의 경우-> constructor -> render -> ref -> componentDidMount
 // -> (setState/props 바뀔때 -> shouldComponentUpdate -> render -> componentDidUpdate)
@@ -21,56 +21,6 @@ const computerChoice = (imgCoord) => {
         return v[1] === imgCoord;
     })[0];
 };
-
-const RSP = () => {
-    const [result, setResult] = useState('');
-    const [imgCoord, setImgCoord] = useState(rspCoords.바위);
-    const [score, setScore] = useState(0);
-    const interval = useRef();
-
-    const changeHand = () => {
-        setImgCoord(rspCoords.가위);
-        if(imgCoord === rspCoords.바위){
-        } else if(imgCoord === rspCoords.가위) {
-            setImgCoord(rspCoords.보);
-        } else if(imgCoord === rspCoords.보) {
-            setImgCoord(rspCoords.바위);
-        }
-    };
-
-    const onClickBtn = (choice) => {
-        clearInterval(interval.current);
-        const myScore = scores[choice];
-        const cpuScore = scores[computerChoice(imgCoord)];
-        const diff = myScore - cpuScore;
-        if(diff === 0){
-            setResult('비겼습니다!');
-        } else if([-1, 2].includes(diff)) {
-            setResult('이겼습니다!');
-            setScore((prevScore)=> prevScore + 1);
-        } else {
-                setResult('졌습니다!');
-                setScore((prevScore)=> prevScore - 1);
-        }
-        setTimeout( () => {
-            interval.current = setInterval(changeHand, 100);
-        }, 2000);
-    };
-
-    return (
-        <>
-            <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
-            <div>
-                <button id="rock" className="btn" onClick={()=> onClickBtn('바위')} >바위</button>
-                <button id="scissor" className="btn" onClick={()=> onClickBtn('가위')} >가위</button>
-                <button id="paper" className="btn" onClick={()=> onClickBtn('보')} >보</button>
-            </div>
-            <div>{result}</div>
-            <div>현재 {score}점</div>
-        </>
-
-    );
-}
 
 class RSP extends Component {
     state = {
