0e6cc2075a2b0cc961f4aa19ebd9a3a59697a6af
myeonghuSong <21300390@handong.edu>
2019-09-11 17:14:49 +0900

    webpack 및 초기 파일 설정

---
+++ MineSearch/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, //입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env',{
+                        targets: {
+                            browsers: ['> 1% in KR'], // browserslist
+                        },
+                        debug: true,
+                    }], 
+                    '@babel/preset-react'],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-hot-loader/babel',
+                ],
+            },
+        }],
+    },
+    
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist/',
+    }, //출력
+
+};
