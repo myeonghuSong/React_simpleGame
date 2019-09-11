d55bbca257cccd6d08bcb2002f304253e8260cce
myeonghuSong <21300390@handong.edu>
2019-09-11 16:19:36 +0900

    TicTacToe 구현
    useState가 많아지면 변수와 setState 를 state와 dispatch에 모아서 한번에 처리하기 위하여 useReducer를 사용
    state를 모아서 action을 통해서만 바꿈 이때 reducer에 정의된대로 바꿈

--- TicTacToe/Td.jsx
+++ TicTacToe/Td.jsx
@@ -1,13 +1,14 @@
 import React, { useCallback } from 'react';
 import CLICK_CELL from './TicTacToe';
-import CHANGE_TURN from './TicTacToe';
-
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
     const onClickTd = useCallback( () => {
         console.log(rowIndex, cellIndex);
+        if(cellData){
+            return;
+        }
         dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex});
-        dispatch({ type:CHANGE_TURN });
-    }, []);
+
+    }, [cellData]);
     
     return (
         <td onClick={onClickTd}>{cellData}</td>
--- TicTacToe/TicTacToe.jsx
+++ TicTacToe/TicTacToe.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useReducer, useCallback } from 'react';
+import React, { useState, useReducer, useCallback, useEffect } from 'react';
 import Table from './Table';
 
 const initialState = {
     winner: '',
     turn: '0',
-    tableData: [['','',''],['','',''],['','','']]
+    tableData: [['','',''],['','',''],['','','']],
+    recentCell : [-1, -1],
 };
 
 export const SET_WINNER = 'SET_WINNER';
 export const CLICK_CELL = 'CLICK_CELL';
 export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -26,6 +28,7 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 tableData,
+                recentCell: [action.row, action.cell],
             }
         }
         case CHANGE_TURN : {
@@ -34,24 +37,73 @@ const reducer = (state, action) => {
                 turn: state.turn === 'O' ? 'X' : 'O',
             }
         }
+        case RESET_GAME : {
+            return {
+                winner: '',
+                turn: '0',
+                tableData: [['','',''],['','',''],['','','']],
+                recentCell : [-1, -1],
+            }
+        }
+        default: 
+            return state;
     }
 };
 
 const TicTacToe = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    
+    const { tableData, turn, winner } = state;
     // const [winner, setWinner] = useState('');
     // const [turn, setTurn] = useState(0);
     // const [tableData, setTableData] = useState([['','',''],['','',''],['','','']]);
 
     const onClickTable = useCallback( () => {
         dispatch({ type: SET_WINNER, winner: '0' });
+        dispatch({ type: RESET_GAME });
     }, []);
+
+    useEffect( () => {
+        const [row,cell] = recentCell;
+        if(row < 0){
+            return;
+        }    
+        let win = false;
+        if( tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn){
+            win = true;
+        }
+        if( tableData[0][cell] === turn && tableData[1][cell] === turn && tableData[2][cell] === turn){
+            win = true;
+        }
+        if( tableData[0][0] === turn && tableData[1][1] === turn && tableData[2][2] === turn){
+            win = true;
+        }
+        if( tableData[0][2] === turn && tableData[1][1] === turn && tableData[2][0] === turn){
+            win = true;
+        }
+        
+        if(win) { //승리시
+            dispatch({ type: SET_WINNER, winner: turn });
+        } else { //무승부 검사
+            let all = true; //all이 true면 무승부라는 뜻
+            tableData.forEach( (row) => {
+                row.forEach( (cell) => {
+                    if(!cell){
+                        all = false;
+                    }
+                });
+            });
+            if(all) {
+                dispatch({ type: RESET_GAME });        
+            } else {
+                dispatch({ type:CHANGE_TURN });
+            }
+        }
+    }, [recentCell])
 
     return (
         <>
-            <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}/>
-            {state.winner && <div>{state.winner}님의 승리</div>}
+            <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch}/>
+            {winner && <div>{winner}님의 승리</div>}
         </>
     )
 }
